3c21917c6d033aa1aa4fa9a9cc78d87761e54151
khanhnq <khanhnq@runsystem.net>
2024-07-09 14:34:57 +0700

    init

---
+++ src/main/java/com/example/gen_code_ai/GenCodeAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gen_code_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenCodeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenCodeAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gen_code_ai/GenCodeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gen_code_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenCodeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
